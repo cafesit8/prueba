08a84f09f38ad823da11c95c3b7543761af2a982
leliasgomer <leliasgomer@gmail.com>
2023-07-26 14:51:57 -0500

    fix: :bug: master add end at the navlink

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
 import Root from './layout/Root'
-import Home from './views/Home'
-import Continents from './views/Continents'
+// import Home from './views/Home'
+// import Continents from './views/Continents'
 import Countries from './views/Countries'
 import Content from './layout/Content'
+import { lazy } from 'react'
+const Home = lazy(() => import('./views/Home'))
+const Continents = lazy(() => import('./views/Continents'))
 
 function App() {
   return (
     <Routes>
       <Route element={<Root />}>
-        <Route path='/prueba' element={<Home />} />
+        <Route path='/prueba/' element={<Home />} />
         <Route element={<Content />}>
-          <Route path='/continents' element={<Continents />} />
-          <Route path='/countries' element={<Countries />} />
+          <Route path='/prueba/continents' element={<Continents />} />
+          <Route path='/prueba/countries' element={<Countries />} />
         </Route>
       </Route>
     </Routes>
